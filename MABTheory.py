bd4413e3db250f046ac6d926a9c280a1234378c0
Timothy Flavin <BinaryOwlizard@gmail.com>
2025-02-12 16:23:34 -0600

    Updated sampler and MATCH... On the way to fully implemented

--- MABTheory.py
+++ MABTheory.py
@@ -117,6 +117,7 @@ class Thompson_Beta_CombiSemi(MAB_Sampler):
     ):
         self.id = id
         self.exp_strength = experience_strength
+        self.prior_strength = prior_strength
         self.single = single
         self.n_agents = n_agents
         self.decay_factor = decay_factor
@@ -125,7 +126,7 @@ class Thompson_Beta_CombiSemi(MAB_Sampler):
         self.avg_num_arms_pulled = n_agents / 2  # Running estimate
         self.arm_pul_probs = np.ones(self.n_agents)
 
-    def choose(self, available_teammates):
+    def choose(self, available_teammates, prior=None):
         """Samples success probabilities and selects teammates to suggest to."""
         # Needs to be bool TODO
         available_teammates = np.array(available_teammates, dtype=bool)
@@ -133,6 +134,12 @@ class Thompson_Beta_CombiSemi(MAB_Sampler):
         # print("before avail: ", sampled_probs)
         # print(available_teammates)
         sampled_probs[np.logical_not(available_teammates)] = 0
+
+        if prior is not None:
+            sampled_probs = (
+                self.exp_strength * sampled_probs + prior * self.prior_strength
+            )
+            sampled_probs = sampled_probs / (self.exp_strength + self.prior_strength)
         # print("after avail: ", sampled_probs)
         # input()
         # print("alpha beta: ", self.alpha, self.beta)
@@ -213,9 +220,14 @@ class Thompson_Gaussian_Sleepy(MAB_Sampler):
         self.alphas = np.zeros(self.num_arms) + alpha_0
         self.prob_weight_arms = prob_weight_arms
 
+        self.prior_strength = prior_strength
+        self.experience_strength = experience_strength
+        self.alpha_0 = alpha_0
+        self.beta_0 = beta_0
+
         self.arm_avail_probs = np.ones(self.num_arms)
 
-    def choose(self, available_teammates, n_options):
+    def choose(self, available_teammates, priors=None):
         """Samples means and selects teammates to suggest to."""
         available_teammates = available_teammates.copy().astype(int)
         available_teammates[self.id] = 1
@@ -236,6 +248,9 @@ class Thompson_Gaussian_Sleepy(MAB_Sampler):
         # print(sampled_means)
         # print(available_teammates)
         scores = sampled_means - (1 - available_teammates) * 1e10
+
+        if priors is not None:
+            scores = self.experience_strength * scores + self.prior_strength * priors
         # print(scores)
         arm = np.argmax(scores)
         # print(arm)
@@ -243,7 +258,7 @@ class Thompson_Gaussian_Sleepy(MAB_Sampler):
         # input()
         return arm
 
-    def update(self, arms_pulled, advantage, n_options):
+    def update(self, arms_pulled, advantage):
         # Apply time decay to previously active arms
         # for a in range(self.num_arms):
         #    if a != arm:
@@ -254,13 +269,16 @@ class Thompson_Gaussian_Sleepy(MAB_Sampler):
         # print("normal means before", self.mu)
         # print("normal sigmas before", invgamma.mean(a=self.alphas, scale=self.betas))
         # input()
-        self.alphas *= self.gamma
+        self.alphas *= self.gamma  # TODO cant shrink past 1, also this feels wrong
         self.betas *= self.gamma
 
         arms_pulled = np.array(arms_pulled).astype(bool)
         for i in range(self.num_arms):
             if arms_pulled[i] < 0.99:
                 self.betas[i] /= self.gamma
+
+        self.alphas = np.max(self.alphas, self.alpha_0)
+        self.betas = np.max(self.betas, self.beta_0)
         # self.betas /= self.gamma  # Increase uncertainty for inactive arms
         # print("what is this nonsense")
         # print(self.lambda_ / self.arm_avail_probs)
@@ -278,9 +296,14 @@ class Thompson_Gaussian_Sleepy(MAB_Sampler):
         prev_mu = self.mu[arms_pulled]
         self.mu[arms_pulled] = (1 - muscale) * prev_mu + muscale * advantage
         self.alphas[arms_pulled] += 0.5 / self.arm_avail_probs[arms_pulled]
-        self.betas[arms_pulled] = (1 - muscale) * self.betas[
-            arms_pulled
-        ] + 0.5 * muscale * (advantage - prev_mu) ** 2
+        if True:
+            self.betas[arms_pulled] += (
+                0.5 * (advantage - prev_mu) ** 2 / self.arm_avail_probs[arms_pulled]
+            )
+        else:
+            self.betas[arms_pulled] = (1 - muscale) * self.betas[
+                arms_pulled
+            ] + 0.5 * muscale * (advantage - prev_mu) ** 2
         self.ns[arms_pulled] += 1  # Increment only for pulled arm
         # print("alphas betas after: ", self.alphas, self.betas)
         # input()
@@ -417,56 +440,77 @@ def check_baseline(n_agents, trials=1000):
 
 
 class MATCH:
-    def __init__(self, n_agents, id=0, stype="Thompson", single=True):
+    def __init__(
+        self,
+        n_agents,
+        id=0,
+        stype="Thompson",
+        single=True,
+        lambda_=0.99,
+        gamma=0.95,
+        arm_lr=0.95,
+        listen_to_duplicate=True,
+    ):
         if stype == "Thompson":
-            self.listener = Thompson_Beta_CombiSemi(
+            self.listener = Thompson_Gaussian_Sleepy(
                 n_agents=n_agents,
-                decay_factor=0.95,
+                gamma=gamma,
+                lambda_=lambda_,
                 prior_strength=1,
                 experience_strength=1,
-                speaker=False,
                 id=id,
-                single=single,
-                n_explore=2,
+                bg_learning_rate=arm_lr,
             )
             self.speaker = Thompson_Beta_CombiSemi(
                 n_agents=n_agents,
-                decay_factor=0.95,
+                decay_factor=gamma,
                 prior_strength=1,
                 experience_strength=1,
-                speaker=True,
                 id=id,
                 single=single,
                 n_explore=2,
             )
-        elif stype == "UCB":
-            self.listener = UCB_Multinomial(n=n_agents, explore_n=2)
-            self.speaker = UCB_Multinomial(n=n_agents, explore_n=2)
-
+        else:
+            print("not implemented")
         self.type = stype
         self.id = id
         self.priors = np.zeros(n_agents) + 2
         self.n_agents = n_agents
+        self.commanded_by = np.zeros(n_agents)
+        self.commanded_by[self.id] = 1
+        self.listen_to_duplicate = listen_to_duplicate
 
-    def policy_with_oracle(self, active, actions):
-        commander_choices = np.copy(active)
-        self.avail_commands = np.copy(active)
-        commander_choices[self.id] = 1
-        leader = self.listener.choose(0.5, active=commander_choices)
-        self.leader = leader
-        chosen_action = actions[leader]
+        # These two vars are memorized during action selection
+        # so that they can be used during update.
+        self.told_to = np.zeros(n_agents)
+        self.n_options = 1
+
+    def policy_with_oracle(self, commanded_by, told_to, priors=None):
+        self.told_to = np.copy(told_to)
+        self.commanded_by = np.copy(commanded_by)
+        self.n_options = np.sum(commanded_by)
+        self.commanded_by[self.id] = 1
+        leader = self.listener.choose(
+            available_teammates=self.commanded_by, priors=priors
+        )
+
+        self.leaders = np.zeros(self.n_agents)
+        self.leaders[leader] = 1
+        if self.listen_to_duplicate:
+            self.leaders[told_to == told_to[leader]] = 1
+            # TODO for continuous actions, add an epsilon for similar actions
+        chosen_action = told_to[leader]
         return chosen_action, leader
 
     def update_listener(self, adv, verbose=0):  # adv is one number
-        if self.avail_commands[self.leader] > 0:
-            self.listener.update(adv, self.leader, verbose)
+        self.listener.update(arms_pulled=self.leaders, advantage=adv)
 
-    def choose_target(self, priors, active):
+    def choose_target(self, available_teammates, priors=None):
         if priors is None:
             priors = self.priors
-        self.target = self.speaker.choose(priors, active)
-        targets = np.zeros(self.n_agents)
-        targets[self.target] = 1
+        self.targets = self.speaker.choose(
+            available_teammates=available_teammates, priors=priors
+        )
         return targets
 
     def update_speaker(
@@ -526,15 +570,20 @@ def test_thompsons(n_agents, n_steps, n_trials):
     speaker_multi_arms_chosen = np.zeros((n_agents, n_steps, n_trials))
     listener_arms_chosen = np.zeros((n_agents, n_steps, n_trials))
     listener_arms_appeared = np.zeros((n_agents, n_steps, n_trials))
+
+    lowest_arm = np.argmin(reward_loc)
+    lowest_val = np.min(reward_loc)
     for j in range(n_trials):
+        reward_loc[lowest_arm] = lowest_val
         speaker_single = Thompson_Beta_CombiSemi(
             n_agents, id=0, single=True, decay_factor=0.99
         )
         speaker_multi = Thompson_Beta_CombiSemi(
             n_agents, id=0, single=False, decay_factor=0.99
         )
-        listener = Thompson_Gaussian_Sleepy(n_agents, id=0, lambda_=0.99, gamma=0.99)
+        listener = Thompson_Gaussian_Sleepy(n_agents, id=0, lambda_=0.99, gamma=0.95)
         for i in range(n_steps):
+            reward_loc[lowest_arm] = lowest_val + max(i * 2 / n_steps, 1) * 3
             available_teammates = np.random.rand(n_agents) < appear_prob
             listener_arms_appeared[:, i, j] = available_teammates
             listener_arms_appeared[0, i, j] = 1  # ID 0 is always available
